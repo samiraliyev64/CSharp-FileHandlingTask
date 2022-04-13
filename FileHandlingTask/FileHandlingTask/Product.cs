d7391b0de90190b1df02a95f313d5e7aa2becbad
samiraliyev64 <70ey3w0@code.edu.az>
2022-04-14 02:13:13 +0400

    task added

---
+++ FileHandlingTask/FileHandlingTask/Product.cs
@@ -0,0 +1,16 @@
+﻿namespace FileHandlingTask
+{
+    class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(int id, string name, double price)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+        }
+    }
+}
---
+++ FileHandlingTask/FileHandlingTask/Program.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+
+namespace FileHandlingTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task 1
+            //Directory.CreateDirectory(@"C:\Users\HP\Desktop\newFolder");
+            //File.Create(@"C:\Users\HP\Desktop\newFolder\myTxtFile.txt");
+
+            //StreamWriter newStreamWriter = new StreamWriter(@"C:\Users\HP\Desktop\newFolder\myTxtFile.txt");
+            //newStreamWriter.WriteLine("bu bir metndir");
+            //newStreamWriter.Close();
+            #endregion
+
+            #region Task 2
+            Product product1 = new Product(1, "Samsung", 700);
+
+            //Serialize
+            var JSONproduct1 = JsonConvert.SerializeObject(product1);
+            Console.WriteLine(JSONproduct1);
+
+            //Deserialize
+            var deserializedProduct = JsonConvert.DeserializeObject(JSONproduct1);
+            Console.WriteLine(deserializedProduct);
+            #endregion
+        }
+    }
+}
